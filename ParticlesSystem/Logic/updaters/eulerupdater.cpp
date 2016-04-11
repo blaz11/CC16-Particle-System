03a9a86aa0adde05ced8b4bbed5a5b3647a91163
Łukasz Ławniczak <lawniczakl@student.mini.pw.edu.pl>
2016-04-11 16:22:35 +0200

    bug fix update when no collision

--- ParticlesSystem/Logic/updaters/eulerupdater.cpp
+++ ParticlesSystem/Logic/updaters/eulerupdater.cpp
@@ -1,7 +1,7 @@
 #include "eulerupdater.h"
+#include <qdebug.h>
 #include <glm/gtx/norm.hpp>
-
-using namespace glm;
+#include "glm/ext.hpp"
 
 void EulerUpdater::update(double dt, ParticleData *p)
 {
@@ -45,16 +45,22 @@ void PPCollisionDetector::updateAll()
             }
         }
         if(collTime < dt)
+        {
             handleCollision(collTime, i, colliding);
-        skipToEnd(dt - collTime - touchTime, i);
+            skipToEnd(dt-collTime-touchTime, i);
+        }
+        else
+        {
+            skipToEnd(dt, i);
+        }
     }
 }
 
 //Solving equation at^2+bt+c = (2R)^2
 double PPCollisionDetector::collisionTime(size_t i, size_t j)
 {
-    vec3 dp = vec3(p->m_pos[i] - p->m_pos[j]);
-    vec3 dv = vec3(p->m_vel[i] - p->m_vel[j]);
+    glm::vec3 dp = glm::vec3(p->m_pos[i] - p->m_pos[j]);
+    glm::vec3 dv = glm::vec3(p->m_vel[i] - p->m_vel[j]);
 
     double a = length2(dv);
     double b = 2 * dot(dp, dv);
@@ -69,30 +75,31 @@ double PPCollisionDetector::collisionTime(size_t i, size_t j)
     double x2 = (-b+sqrt(delta))/(2*a);
 
     //select lower non negative
-    return min(x1,x2);
+    return glm::min(x1,x2);
 }
 
-vec4 addVectors(vec4 a, vec4 b)
+glm::vec4 addVectors(glm::vec4 a, glm::vec4 b)
 {
-    return vec4(a.x+b.x, a.y+b.y,a.z+b.z, a.w + b.w);
+    return glm::vec4(a.x+b.x, a.y+b.y,a.z+b.z, a.w + b.w);
 }
 
 void PPCollisionDetector::handleCollision(double t, size_t i, size_t j)
 {
-    vec3 pos1 = p->positionAtTime(i, t);
-    vec3 pos2 = p->positionAtTime(j, t);
-    vec3 dp = vec3(pos1 - pos2);
+    qDebug() << "Crash (" << i <<", " << j << ") @ "<<t;
+    glm::vec3 pos1 = p->positionAtTime(i, t);
+    glm::vec3 pos2 = p->positionAtTime(j, t);
+    glm::vec3 dp = glm::vec3(pos1 - pos2);
     normalize(dp);
-    vec4 oldVel = p->m_vel[j];
-    vec3 dv = vec3(p->m_vel[i].x - p->m_vel[j].x,
+    glm::vec4 oldVel = p->m_vel[j];
+    glm::vec3 dv = glm::vec3(p->m_vel[i].x - p->m_vel[j].x,
                    p->m_vel[i].y - p->m_vel[j].y,
                    p->m_vel[i].z - p->m_vel[j].z);
-    vec3 normalizedDv = dv;
+    glm::vec3 normalizedDv = dv;
     normalize(normalizedDv);
 
-    p->m_vel[i] = vec4(dv,1);
+    p->m_vel[i] = glm::vec4(dv,1);
     p->m_vel[i] *= dot(dp, normalizedDv);
-    p->m_vel[j] = vec4(dv,1);
+    p->m_vel[j] = glm::vec4(dv,1);
     p->m_vel[j] *= length(cross(dp, normalizedDv));
 
     p->m_vel[i] = addVectors(p->m_vel[i], oldVel);
@@ -103,9 +110,8 @@ void PPCollisionDetector::skipToEnd(double dt, size_t i)
 {
     if(dt > 0)
     {
-        vec4 tmp = p->m_vel[i];
+        glm::vec4 tmp = p->m_vel[i];
         tmp *= dt;
-        p->m_pos[i] = vec4(p->m_pos[i].x + tmp.x, p->m_pos[i].y + tmp.y,
-                           p->m_pos[i].z + tmp.z, p->m_pos[i].w + tmp.w);
+        p->m_pos[i] = addVectors(tmp, p->m_pos[i]);
     }
 }
