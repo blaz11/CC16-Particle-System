ca23588fdfbf436c2a120705e3312cb3cd68de78
kowalkowskim <kowalkowskim@student.mini.pw.edu.pl>
2016-04-11 16:18:41 +0200

    Więcej gui.

--- ParticlesSystem/Logic/generators/timegenerator.cpp
+++ ParticlesSystem/Logic/generators/timegenerator.cpp
@@ -8,5 +8,5 @@ TimeGenerator::TimeGenerator()
 void TimeGenerator::generate(double dt, ParticleData *p, size_t startId, size_t endId)
 {
     for(size_t i=startId;i<endId;++i)
-        p->m_time[i] = 5;
+        p->m_time[i] = 100;
 }
--- ParticlesSystem/mainwindow.cpp
+++ ParticlesSystem/mainwindow.cpp
@@ -12,3 +12,38 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_horizontalSlider_sliderMoved(int position)
+{
+    qDebug() << position;
+}
+
+void MainWindow::on_horizontalSlider_2_sliderMoved(int position)
+{
+    qDebug() << position;
+}
+
+void MainWindow::on_spinBox_valueChanged(int arg)
+{
+    qDebug() << arg;
+}
+
+void MainWindow::on_spinBox_2_valueChanged(int arg)
+{
+    qDebug() << arg;
+}
+
+void MainWindow::on_spinBox_3_valueChanged(int arg)
+{
+    qDebug() << arg;
+}
+
+void MainWindow::on_spinBox_4_valueChanged(int arg)
+{
+    qDebug() << arg;
+}
+
+void MainWindow::on_spinBox_5_valueChanged(int arg)
+{
+    qDebug() << arg;
+}
